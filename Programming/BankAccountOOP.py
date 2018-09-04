069bb3a26c65610b6fc79e2ecc8f71485f83dd68
Erick Cruz Figueiredo <42284872+erickcruzf@users.noreply.github.com>
2018-09-04 12:08:39 -0300

    OOP exercises

---
+++ Programming/BankAccountOOP.py
@@ -0,0 +1,16 @@
+class BankAccount:
+    
+    def __init__(self, owner, balance=0.0):
+        self.owner = owner
+        self.balance = balance
+        
+    def deposit(self, number):
+        self.balance += number
+        return self.balance
+        
+    def withdraw(self, number):
+        self.balance -= number
+        return self.balance
+    
+    def getbalance(self):
+        return self.balance

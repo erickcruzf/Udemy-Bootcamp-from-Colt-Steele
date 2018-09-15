87fa99833dbfebc90192695ce66be6397665cb5e
Erick Cruz Figueiredo <42284872+erickcruzf@users.noreply.github.com>
2018-09-15 00:36:15 -0300

    CSV work!

---
+++ CSVwork.py
@@ -0,0 +1,37 @@
+import csv
+ 
+def addUser(firstName, lastName):
+    with open("users.csv", "a") as csvFile:
+        csvWriter = csv.writer(csvFile)
+        csvWriter.writerow([firstName, lastName])
+
+def printUsers():
+    with open("users.csv") as csvFile:
+        csvReader = csv.DictReader(csvFile)
+        for row in csv_reader: 
+            print(f"{row['First Name']} {row['Last Name']}")
+
+def findUser(firstName, lastName):
+    with open ("users.csv") as csvFile:
+        csvReader = csv.reader(csvFile)
+        for (index, row) in enumerate(csvReader):
+            firstMatch = firstName == row[0]
+            lastMatch = lastName == row[1]
+            if firstMatch and lastMatch:
+                return index
+        return "{} {} not found.".format(firstName, lastName)
+
+def update_users(oldFirst, oldLast, newFirst, newLast):
+    with open("users.csv") as csvFile:
+        csvReader = list(csv.reader(csvFile))
+        
+    update = 0
+    with open("users.csv", "w") as csvFile:
+        csvWriter = csv.writer(csvFile)
+        for row in csvReader:
+            if row[0] == oldFirst and row[1] == oldLast:
+                update += 1
+                csvWriter.writerow([newFirst, newLast])
+            else:
+                csvWriter.writerow(row)
+        return "Users updated: {}.".format(update)

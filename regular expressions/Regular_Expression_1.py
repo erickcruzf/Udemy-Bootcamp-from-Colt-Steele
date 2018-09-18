392d85e04ad582487353a3a89ea59e64513538a4
Erick Cruz Figueiredo <42284872+erickcruzf@users.noreply.github.com>
2018-09-18 20:54:21 -0300

    updated examples comments

--- regular expressions/Regular_Expression_1.py
+++ regular expressions/Regular_Expression_1.py
@@ -1,6 +1,6 @@
 # Don't forget to import re!
 import re
-# time format example = "10:45".
+# time format example = PM "10:45", AM "3:05".
 def is_valid_time(input):
     is_time = re.compile(r"^[AP]M ([0-9]|1[0-2]):(0[1-9]|[1-5][0-9])$")
     find = is_time.search(input)

121753548367687947c9779de01395566a7aea2f
Erick Cruz Figueiredo <42284872+erickcruzf@users.noreply.github.com>
2018-09-18 20:51:24 -0300

    Added one file

---
+++ regular expressions/Regular_Expression_1.py
@@ -0,0 +1,9 @@
+# Don't forget to import re!
+import re
+# time format example = "10:45".
+def is_valid_time(input):
+    is_time = re.compile(r"^[AP]M ([0-9]|1[0-2]):(0[1-9]|[1-5][0-9])$")
+    find = is_time.search(input)
+    if find:
+        return True
+    return False
